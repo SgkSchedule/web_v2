32482454b2b10bdf34855c93d6aa283bfc7dec45
Maksim Vorontsov <maksim_789456@mail.ru>
2022-04-24 04:46:31 +0400

    Try to fix loading of resources x2

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  // publicPath: process.env.NODE_ENV === 'production' ? 'web_v2' : '/'
-  publicPath: '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/web_v2' : '/'
 })
