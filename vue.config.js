5b34e8bb9838c9ba4b6c37a9600c6438ea52561b
Maksim Vorontsov <maksim_789456@mail.ru>
2022-05-01 12:00:23 +0400

    Fix PWA manifest

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'production' ? '/web_v2' : '/'
+  publicPath: process.env.NODE_ENV === 'production' ? '/web_v2' : '/',
+  pwa: {
+    manifestOptions: {
+      name: 'СГК Расписание',
+      short_name: 'СГК Расписание',
+      theme_color: '#1620A5'
+    },
+    themeColor: '#1620A5',
+    msTileColor: '#000000'
+  }
 })
