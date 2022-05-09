29725d3743369253de608e094f87c1e2f5692d3b
Maksim Vorontsov <maksim_789456@mail.ru>
2022-05-09 19:28:10 +0400

    Fix tailwind gray colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,22 +4,27 @@ module.exports = {
     './src/**/*.{vue,js,ts,jsx,tsx}',
     './node_modules/flowbite/**/*.js'
   ],
+  darkMode: 'class',
   theme: {
+    extend: {
+      colors: {
+        gray: {
+          50: '#fafafa',
+          100: '#f4f4f5',
+          200: '#e4e4e7',
+          300: '#d4d4d8',
+          400: '#a1a1aa',
+          500: '#71717a',
+          600: '#52525b',
+          700: '#3f3f46',
+          800: '#27272a',
+          900: '#18181b'
+        }
+      }
+    },
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
-      gray: {
-        50: '#fafafa',
-        100: '#f4f4f5',
-        200: '#e4e4e7',
-        300: '#d4d4d8',
-        400: '#a1a1aa',
-        500: '#71717a',
-        600: '#52525b',
-        700: '#3f3f46',
-        800: '#27272a',
-        900: '#18181b'
-      },
       white: '#ffffff',
       'light-background': '#f5f5f4'
     },
